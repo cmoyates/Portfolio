6deed92bc7cd534af78c5f8e2dfe41dc330a5ccd
cmoyates <cmoyates@gmail.com>
2022-06-12 00:42:09 -0230

    Refactored the Home view into its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import "./global.css";
-  import Button from "./lib/Button.svelte";
+  import Home from "./lib/views/Home.svelte";
   import { tweened } from "svelte/motion";
   import { cubicOut } from 'svelte/easing';
 
@@ -17,7 +17,8 @@
 <main class="
   bg-black 
   min-h-screen 
-  flex flex-col 
+  flex 
+  flex-col 
   justify-center 
   items-center
   p-4
@@ -26,24 +27,14 @@
   <div style="max-width: {$width}rem; max-height: {$height}rem" class="
     text-xl 
     bg-slate-100
-    w-full 
-    max-h-72 
+    w-full
     h-screen 
     flex 
     flex-col
-    justify-evenly
+    justify-center
     items-center
     rounded-3xl
   ">
-    <h1 class="text-3xl">Hi, my name is Cris</h1>
-    <div class="
-      flex
-      flex-row
-      space-x-5
-    ">
-      <Button text={"Projects"} click={() => {width.set(106); height.set(60);}}/>
-      <Button text={"GitHub"} click={() => {width.set(28); height.set(18);}}/>
-      <Button text={"Contact"} click={() => {width.set(38); height.set(18);}}/>
-    </div>
+    <Home width={width} height={height}/>
   </div>
 </main>
---
+++ src/lib/views/Home.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Button from "../components/Button.svelte";
+    import type { Tweened } from "svelte/motion";
+
+    export let width: Tweened<number>;
+    export let height: Tweened<number>;
+</script>
+
+<div class="
+    flex 
+    flex-col
+    justify-evenly
+    items-center
+    h-full 
+">
+    <h1 class="text-3xl">Hi, my name is Cris</h1>
+    <div class="
+      flex
+      flex-row
+      space-x-5
+    ">
+      <Button text={"Projects"} click={() => {width.set(106); height.set(60);}}/>
+      <Button text={"GitHub"} click={() => {width.set(28); height.set(18);}}/>
+      <Button text={"Contact"} click={() => {width.set(38); height.set(18);}}/>
+    </div>
+</div>
