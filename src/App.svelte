54387295d4791e7e62ca844f7c99474ad5f0e23c
cmoyates <cmoyates@gmail.com>
2022-06-11 22:20:42 -0230

    Remade the home view

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
   import "./global.css"
+  import Button from "./lib/Button.svelte";
 </script>
 
-<main>
-  <h1>Hello Typescript!</h1>
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+<main class="bg-black min-h-screen flex flex-col justify-center items-center">
+  <div class="
+    text-xl 
+    bg-slate-100 
+    max-w-md 
+    w-full 
+    max-h-72 
+    h-screen 
+    flex 
+    flex-col
+    justify-evenly
+    items-center
+    rounded-3xl
+    p-6
+  ">
+    <h1 class="text-3xl">Hi, my name is Cris</h1>
+    <div class="
+      flex
+      flex-row
+      space-x-5
+    ">
+      <Button text={"Projects"} click={() => {console.log("sjkslhfd")}}/>
+      <Button text={"GitHub"} click={() => {console.log("sjkslhfd")}}/>
+      <Button text={"Contact"} click={() => {console.log("sjkslhfd")}}/>
+    </div>
+  </div>
 </main>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let text: string;
+    export let click: Function;
+</script>
+
+<button on:click={click()} class="
+    rounded-sm
+    bg-white
+    drop-shadow-md
+    px-6
+    py-2
+    text-sm
+    hover:bg-slate-100
+">
+    {text}
+</button>
