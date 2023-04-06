2a97db342eeb7c3a001731bc1b341b0c4b4086fb
Cristopher Yates <cmoyates@gmail.com>
2023-04-06 04:32:05 -0230

    Updated to SvelteKit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import postcss from './postcss.config.cjs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  css:{
-    postcss
-  }
-})
+	plugins: [sveltekit()]
+});
